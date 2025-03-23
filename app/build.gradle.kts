b52ef788781efda49787b7c48158c0fded8fdcd4
Ibrohim Usmonov <ibrohimusmonov365@gmail.com>
2025-03-23 10:36:15 +0500

    change background color and change sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ibrohimapk3.workwithgit"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ibrohimapk3.workwithgit"
